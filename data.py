155d4f1d6c93fa00d0ab42e920d513ef44cf6fa4
Haider Paniagua <Haidpab777@gmail.com>
2026-09-06 20:38:52 -0500

    Actualizada la URL del servidor en data.py

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-40adb702-c27c-46fa-a5f3-efb1c90ee4c6.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-dc7520a9-c203-48da-9593-28e490834ca6.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
